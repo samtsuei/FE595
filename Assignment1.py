c058f72583b3dbba2bb5133c32fbf51e80522ae3
samtsuei <60580822+samtsuei@users.noreply.github.com>
2020-04-16 22:49:10 -0400

    Add files via upload

--- Assignment1.py
+++ Assignment1.py
@@ -1,8 +1,12 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
-x = np.arange(0,2*np.pi,0.1)  
-y = np.sin(x)
-z = np.cos(x)
-plt.plot(x,y,x,z)
-plt.show()
+
+def main():
+ x = np.arange(0, 2 * np.pi, 0.1)
+ y = np.sin(x)
+ z = np.cos(x)
+ plt.plot(x, y, x, z)
+ plt.show()
+
+if __name__ == "__main__":
+ main()
